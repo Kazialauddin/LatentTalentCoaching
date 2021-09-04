1fb51040c48fd37f72cb3f2518869fd847e21a5d
Kazi Alauddin <Kazi.alauddin421@outlook.com>
2021-09-04 16:18:04 +0600

    cm

--- w1/Controllers/Departments1Controller.cs
+++ w1/Controllers/Departments1Controller.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Linq;
-using System.Net;
-using System.Web;
+﻿
 using System.Web.Mvc;
 using w1.Interface;
 using w1.Models;
